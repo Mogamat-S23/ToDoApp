4ba90b38807f75f875c82045f66f1bc3b474c6eb
Mogamat-S23 <saleemariefdien@gmail.com>
2022-06-24 14:17:25 +0200

    second push

--- script/code.js
+++ script/code.js
@@ -21,7 +21,7 @@ function addTodo(item) {
     if (item !== '') {
       // make a todo object, which has id, name, and completed properties
       const todo = {
-        id: 1,
+        id: Date.now(),
         name: item,
         completed: false
       };
@@ -32,3 +32,43 @@ function addTodo(item) {
       todoInput.value = '';
     }
   }
+
+  // function to display given todos to screen
+function renderTodos(todos) {
+    // clear everything inside <ul> with class=todo-items
+    todoItemsList.innerHTML = '';
+  // run through each item inside todos
+    todos.forEach(function(item) {
+      // check if the item is completed
+      const checked = item.completed ? 'checked': null;
+  // make a <li> element and fill it
+      // <li> </li>
+      const li = document.createElement('li');
+      // <li class="item"> </li>
+      li.setAttribute('class', 'item');
+    
+      li.setAttribute('data-key', item.id);
+     
+      // if item is completed, then add a class to <li> called 'checked', which will add line-through style
+      if (item.completed === true) {
+        li.classList.add('checked');
+      }
+  li.innerHTML = `
+        <input type="checkbox" class="checkbox" ${checked}>
+        ${item.name}
+        <button class="delete-button">X</button>
+      `;
+      //  add the <li> to the <ul>
+      todoItemsList.append(li);
+    });
+  }
+
+  // add todos to local storage
+function addToLocalStorage(todos) {
+    // conver the array to string then store it.
+    localStorage.setItem('todos', JSON.stringify(todos));
+    // display them to screen
+    renderTodos(todos);
+  }
+
+  
